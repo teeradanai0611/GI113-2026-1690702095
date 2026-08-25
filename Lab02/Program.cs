7858df7cd85d6cfe902f447731c4343d4114046b
teeradanai0611 <teeradanai7689@gmail.com>
2026-08-25 10:39:20 +0700

    Add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID : 1690702095
+ * Name       : Lab02
+ * Section    : 129C
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
