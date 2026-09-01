f598c8e942861e530d495c8f0116a44c0bdf5075
ICQ <teeradanai7689@gmail.com>
2026-09-01 10:56:03 +0700

    Lab03 - Type Conversion Finish

---
+++ Lab03/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Student ID : 1690702095
+ * Name       : Lab03
+ * Section    : 129C
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+using System.Runtime.CompilerServices;
+
+namespace Lab03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName}" +
+                $"\nRank: {rank}" +
+                $"\nLevel: {level} / {MaxLevel}" +
+                $"\nHP: {currentHp} / {maxHp}" +
+                $"\nAttack Power: {attackPower}" +
+                $"\nCrit Multiplier {critMultiplier}" +
+                $"\nIs Boss: {isBoss}");
+            Console.WriteLine();
+            // Implicit Conversion int (HP) -->> double
+            Console.WriteLine("----- Implicit Conversion: HP as double -----");
+            double currentHpDouble = currentHp;
+            Console.WriteLine($"HP (double): {currentHpDouble}");
+
+            // Calculate Percantage
+            Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+            double hpPercentExact = currentHpDouble * 100 / maxHp;
+            Console.WriteLine($"HP Percent (exact): {hpPercentExact}%");
+
+            // Explicit float (attakPower) -->> int
+            Console.WriteLine();
+            Console.WriteLine("----- Explicit Cast: Attack Power -> Display Int -----");
+            int attackDisplay = (int)attackPower;
+            Console.WriteLine($"Attack Power (int cast): {attackDisplay}");
+
+            // Cast vs. Convert double (critMultiplier) -->> int
+            Console.WriteLine();
+            Console.WriteLine("----- Cast vs Convert: Crit Multiplier -----");
+            int critCast = (int)critMultiplier;
+            int critConvert = Convert.ToInt32(critMultiplier);
+            Console.WriteLine($"Crit Multiplier (int cast): {critCast}");
+            Console.WriteLine($"Crit Multiplier(Convert rounded): { critConvert}");
+
+
+
+        }
+    }
+}
